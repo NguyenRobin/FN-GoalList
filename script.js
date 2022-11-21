2eedb91d1a976955c9958c32f4182b4544d8f76b
NguyenRobin <robin.nnguyen@hotmail.com>
2022-11-21 15:20:35 +0100

    targets

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 "use strict";
 
 const goalContainerEl = document.querySelector(".container");
-const btnPlusSign = document.querySelector(".btn-open");
-const goalSection = document.querySelector(".goal-section");
 
-function renderGoal(data) {
+async function renderGoal(data) {
   const html = `
   <article class="goal-section">
     <h2 class="goal-heading">Goal ${data.code}</h2>
     <section class="goal-information">
     <p class="goal-text">${data.title}</p>
     <button class="btn-open"><span><i class="fa-solid fa-plus"></i></span></button>
-    <button><span><i class="fa-solid fa-minus"></i></span></button>
-    </section
+    <button class="btn-close"><span><i class="fa-solid fa-minus"></i></span></button>
+    </section>
   </article>
   `;
 
   goalContainerEl.insertAdjacentHTML("beforeend", html);
+
+  const responseTarget = await fetch(
+    `https://unstats.un.org/SDGAPI/v1/sdg/Goal/${data.code}/Target/List?includechildren=true`
+  );
+  const dataTarget = await responseTarget.json();
+  // console.log(dataTarget);
+  const ul = document.createElement("ul");
+  for (let i = 0; i < dataTarget[0].targets.length; i++) {
+    const li = document.createElement("li");
+    ul.classList.add("target-text");
+    li.textContent = `CODE ${dataTarget[0].targets[i].code} TARGETS: ${dataTarget[0].targets[i].title}`;
+    ul.append(li);
+  }
+  return ul;
 }
 
 async function displayGoal() {
@@ -25,23 +37,23 @@ async function displayGoal() {
       `https://unstats.un.org/SDGAPI/v1/sdg/Goal/List?includechildren=false`
     );
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
     for (const eachData of data) {
-      renderGoal(eachData);
-      const responseTarget = await fetch(
-        `https://unstats.un.org/SDGAPI/v1/sdg/Goal/${eachData.code}/Target/List?includechildren=true`
-      );
-      // console.log(responseTarget.json());
-      const dataTarget = await responseTarget.json();
-
-      const ul = document.createElement("ul");
-      for (let i = 0; i < dataTarget[0].targets.length; i++) {
-        const li = document.createElement("li");
-        li.textContent = `CODE ${dataTarget[0].targets[i].code} TARGETS: ${dataTarget[0].targets[i].title}`;
-        ul.append(li);
-        console.log(ul);
+      const ul = await renderGoal(eachData);
+      let goals = document.querySelectorAll(".goal-information");
+      for (let goal of goals) {
+        goal.after(ul);
       }
-      document.querySelector(".goal-information").append(ul);
+    }
+
+    // CLICK WON'T CHANGE CORRECT .target-text
+    for (let i = 0; i < document.querySelectorAll(".btn-open").length; i++) {
+      const clickedBtn = document.querySelectorAll(".btn-open")[i];
+      clickedBtn.addEventListener("click", function (event) {
+        if (event.currentTarget.closest(".goal-section")) {
+          document.querySelector(".target-text").style.display = "block";
+        }
+      });
     }
   } catch (error) {
     console.log(error.message);
